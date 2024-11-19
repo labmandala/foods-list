4c417aa579d65ba7ea5015122cf848b53311504d
sarah <skg@neofilm.us>
2024-11-18 21:17:28 -0500

    Add separate component for food item, allow deletion, prevent empty item

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import NewFoodInput from "./NewFoodInput";
+import Food from "./Food";
 import "./styles.css";
 import Confetti from "react-confetti";
 
@@ -9,14 +10,21 @@ export default function App() {
   const addFoodItem = (food) => {
     setFoodsToTry([...foodsToTry, food]);
   };
+
+  const deleteFood = (foodToDelete) => {
+    const updatedFoods = foodsToTry.filter((food) => food !== foodToDelete);
+    setFoodsToTry(updatedFoods);
+  };
   return (
     <div className="App">
       <Confetti />
       <h1>New Foods to Try</h1>
       <NewFoodInput addFoodItem={addFoodItem} />
       <ul className="food-list">
-      {foodsToTry.map((food, index) => (
-          <li key={index}>{food}</li>
+        {foodsToTry.map((food, index) => (
+          <Food key={index} food={food} deleteFood={deleteFood} />
+          // You could also use this if you prefer not to create a separate function:
+          // <Food key={index} food={food} deleteFood={() => setFoodsToTry(foodsToTry.filter(foodToDelete => foodToDelete !== food))} />
         ))}
       </ul>
     </div>
